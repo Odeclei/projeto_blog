3a3804016d036adcd0dd55dedd7cd8fa9389884d
Odeclei <odeclei_amanini@hotmail.com>
2023-10-20 15:58:40 -0300

    criado models e gerenciamento do admin para o blog2

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,3 +1,5 @@
+# Flake8: noqa
+
 from django.db import models
 from utils.rands import slugfy_new
 
@@ -43,12 +45,51 @@ class Category(models.Model):
 
 
 class Page(models.Model):
+    class Meta:
+        verbose_name = 'Page'
+        verbose_name_plural = 'Pages'
+
     title = models.CharField(max_length=65)
     slug = models.SlugField(
         unique=True, default=None, null=True, blank=True, max_length=255,
     )
     is_published = models.BooleanField(default=False)
     content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugfy_new(self.title)
+
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        unique=True, default=None, null=True, blank=True, max_length=255,
+    )
+    excerpt = models.CharField(max_length=150)
+    is_published = models.BooleanField(default=False)
+    content = models.TextField()
+    cover = models.ImageField(upload_to='posts/%Y/%m/', blank=True, default='')
+    cover_in_post_content = models.BooleanField(
+        default=True,
+        help_text='Exibir a imagem da capa no conteúdo do Post?',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,
+    )
+
+    tags = models.ManyToManyField(Tag, blank=True, default='')
 
     def save(self, *args, **kwargs):
         if not self.slug:
